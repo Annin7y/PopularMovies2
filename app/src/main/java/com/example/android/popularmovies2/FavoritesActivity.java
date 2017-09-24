022ea7ebea47c07b91932f66493ae244662ac4e8
AnaVox6y <ana_35an@yahoo.ca>
2017-09-24 17:25:44 -0400

    update code

--- app/src/main/java/com/example/android/popularmovies2/FavoritesActivity.java
+++ app/src/main/java/com/example/android/popularmovies2/FavoritesActivity.java
@@ -1,9 +1,13 @@
 package com.example.android.popularmovies2;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 
-public class FavoritesActivity extends AppCompatActivity  {
+public class FavoritesActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String TAG = FavoritesActivity.class.getSimpleName();
 
@@ -13,6 +17,23 @@ public class FavoritesActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_favorites);
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, final Bundle loaderArgs) {
+        return null;
+    }
 
+    public Cursor loadInBackground() {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+
+    }
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader){
+
+    }
 
 }
