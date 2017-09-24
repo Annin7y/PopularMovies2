787f5f641b17b9344395ed784e7ed503c2a2665a
AnaVox6y <ana_35an@yahoo.ca>
2017-09-24 16:57:39 -0400

    update code

---
+++ app/src/main/java/com/example/android/popularmovies2/recyclerviewadapters/FavoritesAdapter.java
@@ -0,0 +1,12 @@
+package com.example.android.popularmovies2.recyclerviewadapters;
+
+/**
+ * Created by Maino96-10022 on 9/24/2017.
+ */
+
+public class FavoritesAdapter {
+
+    private static final String TAG = FavoritesAdapter.class.getSimpleName();
+
+
+}
