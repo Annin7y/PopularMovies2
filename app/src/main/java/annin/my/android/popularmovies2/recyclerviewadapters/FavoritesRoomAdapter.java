664557c131ce2ea3ba094222154708f503f51e99
Maino96-10022 <anast3222@gmail.com>
2019-06-20 07:59:11 -0400

    update code: content provider to room

--- app/src/main/java/annin/my/android/popularmovies2/recyclerviewadapters/FavoritesRoomAdapter.java
+++ app/src/main/java/annin/my/android/popularmovies2/recyclerviewadapters/FavoritesRoomAdapter.java
@@ -17,78 +17,78 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import timber.log.Timber;
 
-//public class FavoritesRoomAdapter extends RecyclerView.Adapter<FavoritesRoomAdapter.FavoritesRoomAdapterViewHolder>
-//        {
-//private static final String TAG = FavoritesRoomAdapter.class.getSimpleName();
-//
-//private Context context;
-//private ArrayList<Movie> roomMoviesList = new ArrayList<Movie>();
-//private MovieAdapter.MovieAdapterOnClickHandler mClickHandler;
-//public static final int IMAGE_HEIGHT = 185;
-//public static final int IMAGE_WIDTH = 50;
-//
-//
-//public FavoritesRoomAdapter(MovieAdapter.MovieAdapterOnClickHandler clickHandler, Context context)
-//        {
-//        mClickHandler = clickHandler;
-//        this.context = context;
-//        }
-//
-//public class FavoritesRoomAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
-//{
-//    @BindView(R.id.imageView)
-//    ImageView imageView;
-//
-//    public FavoritesRoomAdapterViewHolder(View view)
-//    {
-//        super(view);
-//        ButterKnife.bind(this, view);
-//        view.setOnClickListener(this);
-//    }
-//
-//    @Override
-//    public void onClick(View v)
-//    {
-//
-//        int adapterPosition = getAdapterPosition();
-//        Movie posterClick = roomMoviesList.get(adapterPosition);
-//        mClickHandler.onClick(posterClick);
-//
-//    }
-//}
-//
-//    @Override
-//    public void onBindViewHolder(FavoritesRoomAdapter.FavoritesRoomAdapterViewHolder holder, int position)
-//    {
-//        //Binding data
-//        final Movie movieView = roomMoviesList.get(position);
-//
-//        Picasso.with(context)
-//                .load(movieView.getPosterUrl())
-//                .error(R.drawable.user_placeholder_error)
-//                .into(holder.imageView);
-//        // Log.e(TAG, "Failed to load image.");
-//        Timber.e("Failed to load image.");
-//
-//    }
-//
-//    @Override
-//    public FavoritesRoomAdapter.FavoritesRoomAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType)
-//    {
-//        Context context = viewGroup.getContext();
-//        int layoutIdForListItem = R.layout.movie_list_item;
-//        LayoutInflater inflater = LayoutInflater.from(context);
-//        boolean shouldAttachToParentImmediately = false;
-//        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
-//        return new FavoritesRoomAdapter.FavoritesRoomAdapterViewHolder(view);
-//    }
-//
-//            public int getItemCount()
-//            {
-//                if(roomMoviesList != null)
-//                return roomMoviesList.size();
-//                else return 0;
-//            }
-//}
+public class FavoritesRoomAdapter extends RecyclerView.Adapter<FavoritesRoomAdapter.FavoritesRoomAdapterViewHolder>
+        {
+private static final String TAG = FavoritesRoomAdapter.class.getSimpleName();
+
+private Context context;
+private ArrayList<Movie> roomMoviesList = new ArrayList<Movie>();
+private MovieAdapter.MovieAdapterOnClickHandler mClickHandler;
+public static final int IMAGE_HEIGHT = 185;
+public static final int IMAGE_WIDTH = 50;
+
+
+public FavoritesRoomAdapter(MovieAdapter.MovieAdapterOnClickHandler clickHandler, Context context)
+        {
+        mClickHandler = clickHandler;
+        this.context = context;
+        }
+
+public class FavoritesRoomAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+    @BindView(R.id.imageView)
+    ImageView imageView;
+
+    public FavoritesRoomAdapterViewHolder(View view)
+    {
+        super(view);
+        ButterKnife.bind(this, view);
+        view.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+
+        int adapterPosition = getAdapterPosition();
+        Movie posterClick = roomMoviesList.get(adapterPosition);
+        mClickHandler.onClick(posterClick);
+
+    }
+}
+
+    @Override
+    public void onBindViewHolder(FavoritesRoomAdapter.FavoritesRoomAdapterViewHolder holder, int position)
+    {
+        //Binding data
+        final Movie movieView = roomMoviesList.get(position);
+
+        Picasso.with(context)
+                .load(movieView.getPosterUrl())
+                .error(R.drawable.user_placeholder_error)
+                .into(holder.imageView);
+        // Log.e(TAG, "Failed to load image.");
+        Timber.e("Failed to load image.");
+
+    }
+
+    @Override
+    public FavoritesRoomAdapter.FavoritesRoomAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType)
+    {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.movie_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new FavoritesRoomAdapter.FavoritesRoomAdapterViewHolder(view);
+    }
+
+            public int getItemCount()
+            {
+                if(roomMoviesList != null)
+                return roomMoviesList.size();
+                else return 0;
+            }
+}
 
 
